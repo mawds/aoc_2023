4a820396686647933d732060a6e0d2f31d2eef09
David Mawdsley <david@mawds.co.uk>
2023-12-03 19:04:43 +0000

    Part 2 done

--- day03/day03.py
+++ day03/day03.py
@@ -1,6 +1,8 @@
 import re
 
+
 #infile = "day03/data/example.txt"
+
 infile = "day03/data/day03.txt"
 
 with open(infile) as f:
@@ -8,6 +10,17 @@ with open(infile) as f:
 
 indata = [i.strip() for i in indata]
 indata = [i for i in indata if len(i) > 0]
+
+
+def adjacent_coordinates(coordinate):
+    acs = []
+    for x in range(coordinate[0] - 1, coordinate[0] + 2):
+        for y in range(coordinate[1] - 1, coordinate[1] + 2):
+            new_coordinate = (x, y)
+            if new_coordinate != coordinate:
+                acs.append(new_coordinate)
+
+    return acs
 
 
 class Part:
@@ -23,19 +36,9 @@ class Part:
             for i in range(self.start_coordinate[0], self.end_coordinate[0] + 1)
         ]
 
-    def adjacent_coordinates(self, coordinate):
-        acs = []
-        for x in range(coordinate[0] - 1, coordinate[0] + 2):
-            for y in range(coordinate[1] - 1, coordinate[1] + 2):
-                new_coordinate = (x, y)
-                if new_coordinate != coordinate:
-                    acs.append(new_coordinate)
-
-        return acs
-
     def is_part_number(self, symbols):
         for c in self.coordinates:
-            acs = self.adjacent_coordinates(c)
+            acs = adjacent_coordinates(c)
             if len(set(acs) & {s.coordinate for s in symbols}) > 0:
                 return True
 
@@ -46,11 +49,28 @@ class Part:
 
 
 class Symbol:
-    def __init__(self, coordinate):
+    def __init__(self, symb, coordinate):
         self.coordinate = coordinate
+        self.symb = symb
 
     def __str__(self):
-        return f"{self.coordinate}"
+        return f"{self.coordinate}: {self.symb}"
+
+    def adjacent_parts(self, parts):
+        acs = adjacent_coordinates(self.coordinate)
+        ap = []
+        for p in parts:
+            if set(p.coordinates) & set(acs):
+                ap.append(p)
+        
+        return ap
+
+    def get_gear_ratio(self, parts):
+        if self.symb == "*":
+            aps = self.adjacent_parts(parts)
+            if len(aps) == 2:
+                return aps[0].value * aps[1].value
+        return 0
 
 
 parts = []
@@ -64,7 +84,7 @@ for ypos, value in enumerate(indata):
             digit_string += character
         else:
             if character != ".":
-                symbols.append(Symbol((xpos, ypos)))
+                symbols.append(Symbol(character, (xpos, ypos)))
 
             if digit_string != "":
                 parts.append(Part(digit_string, (xpos - 1, ypos)))
@@ -73,9 +93,6 @@ for ypos, value in enumerate(indata):
     if digit_string != "":
         parts.append(Part(digit_string, (xpos - 1, ypos)))
         digit_string = ""
-    
-    
-
 
 
 actual_parts = []
@@ -86,3 +103,9 @@ for p in parts:
         part_sum += p.value
 
 print("Part 1:", part_sum)
+
+ratios = 0
+for s in symbols:
+    ratios += s.get_gear_ratio(parts)
+    
+print("Part 2", ratios)
